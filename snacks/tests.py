b0de231204a00ba2e51fa769c0033ba5971aba85
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-14 18:25:08 +0200

    test: added a test to check status code for 'home'

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class TestSnacks(TestCase):
+    def test_home_status_code(self):
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
